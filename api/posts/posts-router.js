a831eb2d373fe7793c5863cbc673187ea7428435
Anthony Catanzariti <apcatanzariti@gmail.com>
2021-03-09 16:48:59 -0500

    finished with MVP

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -37,6 +37,27 @@ router.get('/:id', (req, res) => {
 });
 
 // GET - /:id/comments
+router.get('/:id/comments', (req, res) => {
+    const id = req.params.id;
+
+    Posts.findById(id)
+    .then(post => {
+        Posts.findPostComments(post.id)
+        .then(comments => {
+            if (comments.length > 0) {
+            res.status(200).json(comments);
+            } else {
+                res.status(200).json({ message: 'No comments yet 😥' });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'The comments information could not be retrieved' });
+        })
+    })
+    .catch(err => {
+        res.status(404).json({ message: 'The post with the specified ID does not exist' });
+    })
+});
 
 // POST - /
 router.post('/', (req, res) => {
